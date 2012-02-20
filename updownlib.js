80e9ecf08b3cb0a8f194c846ac222b669560363b
Charles Sprayberry <cspray@gmail.com>
2012-02-20 16:38:47 -0500

    UPD I was working on stuff yesterday it seems

--- updownlib.js
+++ updownlib.js
@@ -1,132 +1,181 @@
-function getUserRep() {
-    var repElement = $("#hlinks-user .reputation-score");
-    var repScore = repElement.html();
-    if (repScore == null) {
-        repScore = 0;
-    } else {
-        repScore = repScore.replace(/,/, '');
-        repScore = parseInt(repScore);
+function SoUpDown(Api) {
+
+    var StackApi = Api;
+
+    var repLimit = 999;
+
+    var extensionClass = "so-up-and-down";
+
+    var userRepSelector = "#hlinks-user .reputation-score";
+    var voteCountPostSelector = ".vote-count-post";
+
+    // A data attribute storing the id value for the question or answer
+    // We are using this attribute as answer posts do not have a normal id whereas questions do
+    var questionIdAttribute = "data-questionid";
+    var answerIdAttribute = "data-answerid";
+
+    var questionClass = "question";
+    var answerClass = "answer";
+
+    var that = this;
+
+    // Will add the extension's class for identifying q/a vote counts to all
+    // questions and answers in a Stack Exchange site.
+    this.addExtensionClassToVoteCount = function() {
+        if (that.getUserRep() <= repLimit) {
+            $(voteCountPostSelector).addClass(extensionClass);
+        }
+    };
+
+    // Will actually split the single vote score into up/down count
+    // Is the function called by the extension class click event handler
+    this.splitVoteCount = function(post) {
+        var postInfo = getPostInfo(post);
+        StackApi.getVoteCount(postInfo, renderVoteCount);
+    };
+
+    // Will return an integer with the user's reputation if logged in or 0 if not
+    this.getUserRep = function() {
+        var repScore = $(userRepSelector).html();
+        if (repScore == null) {
+            repScore = 0;
+        } else {
+            repScore = parseInt(repScore.replace(/,/, ''));
+        }
+        return repScore;
+    };
+
+    // Returns an object with the 'type', 'id', and 'host' for the q/a to split count of
+    function getPostInfo(post) {
+        var postClass = post.attr('class');
+        var postId = 0;
+        if (postClass == questionClass) {
+            postId = post.attr(questionIdAttribute);
+        }
+        if (postClass == answerClass) {
+            postId = post.attr(answerIdAttribute);
+        }
+        postId = parseInt(postId);
+        var host = window.location.host
+        return {
+            type: postClass,
+            id: postId,
+            host: host
+        };
     }
-    return repScore;
-}
 
-// this CSS class is used to trigger the extension sending off API request
-function addSEUpDownClass(userRep) {
-    if (userRep <= 999) {
-        $(".vote-count-post").addClass("se-up-down");
-    }
-}
+    // Used to input the data for vote count into the appropriate q/a based on the request sent
+    function renderVoteCount(request, data) {
+        var selector = "[data-" + request.type + "id=" + request.id + "] ." + extensionClass;
+        var element = $(selector);
 
-function getPostInfo(post) {
-    var object = {};
-    var postClass = post.attr('class');
-    var postId = 0;
-    if (postClass == "question") {
-        postId = post.attr('data-questionid');
-    }
-    if (postClass == "answer") {
-        postId = post.attr('data-answerid');
-    }
-    object.type = postClass;
-    object.id = parseInt(postId);
-    object.host = window.location.host;
-    return object;
-}
+        var normalizeInfo = function(info) {
+            var data = {};
+            if (info.up_vote_count == undefined || info.up_vote_count == 0) {
+                data.up = 0;
+            } else {
+                data.up = "+" + info.up_vote_count;
+            }
 
-function getStackApi() {
-
-    return new StackApi();
-
-    function StackApi() {
-        this.getVoteCount = function(request) {
-            request.apiUrl = this.getApiUrl(request);
-            request.apiFilter = this.getApiFilter(request);
-            this.queryForVoteCount(request);
+            if (info.down_vote_count == undefined || info.down_vote_count == 0) {
+                data.down = 0;
+            } else {
+                data.down = "-" + info.down_vote_count;
+            }
+            return data;
         };
 
-        this.getApiUrl = function(request) {
-            var apiUrl = "https://api.stackexchange.com/2.0/";
-            if (request.type == "question") {
-                apiUrl = apiUrl + "questions/" + request.id;
-            }
-            if (request.type == "answer") {
-                apiUrl = apiUrl + "answers/" + request.id;
-            }
-            return apiUrl;
-        }
+        var getHtml = function(info) {
+            var html = "";
+            html = html + "<div style=\"color: green;\">" + info.up + "</div>";
+            html = html + "<div class=\"vote-count-separator\"></div>";
+            html = html + "<div style=\"color: maroon;\">" + info.down + "</div>";
+            return html;
+        };
 
-        this.getApiFilter = function(request) {
-            var apiFilter = '';
-            if (request.type == "question") {
-                apiFilter = "!awRehpC1pG2C(t";
-            }
-            if (request.type == "answer") {
-                apiFilter = "!6Jzry)jfEAcuO";
-            }
-            return apiFilter;
-        }
-
-        this.queryForVoteCount = function(request) {
-            var url = request.apiUrl;
-            var apiData = {
-                site: request.host,
-                filter: request.apiFilter,
-                pagesize: 1,
-                page: 1
-            };
-            var selector = "[data-" + request.type + "id=" + request.id + "] .se-up-down";
-            var element = $(selector);
-            var ajaxSettings = {
-                data: apiData,
-                error: function(jqHr, status, error) {
-                    console.log("Show better error message eventually");
-                },
-                success: function(data, status, jqHr) {
-                    if (data.items == undefined || data.items.length == 0) {
-                        console.log("Unexpected error encountered.");
-                        return;
-                    }
-                    var normalizeInfo = function(info) {
-                        var data = {};
-                        if (info.up_vote_count == undefined || info.up_vote_count == 0) {
-                            data.up = 0;
-                        } else {
-                            data.up = "+" + info.up_vote_count;
-                        }
-
-                        if (info.down_vote_count == undefined || info.down_vote_count == 0) {
-                            data.down = 0;
-                        } else {
-                            data.down = "-" + info.down_vote_count;
-                        }
-                        return data;
-                    };
-                    var getHtml = function(info) {
-                        var html = "";
-                        html = html + "<div style=\"color: green;\">" + info.up + "</div>";
-                        html = html + "<div class=\"vote-count-separator\"></div>";
-                        html = html + "<div style=\"color: maroon;\">" + info.down + "</div>";
-                        return html;
-                    }
-                    var info = normalizeInfo(data.items[0]);
-                    element.html(getHtml(info));
-                },
-                url: url
-            }
-            $.ajax(ajaxSettings);
-
-        }
+        var info = normalizeInfo(data.items[0]);
+        element.html(getHtml(info));
     }
 
 }
 
-var userRep = getUserRep();
-addSEUpDownClass(userRep);
-$(".se-up-down").live("click", function() {
-    var post = $(this).parents('table').parent('div');
-    var postInfo = getPostInfo(post);
-    var StackApi = getStackApi();
-    StackApi.getVoteCount(postInfo);
+function StackApi() {
+
+    var apiUrl = "https://api.stackexchange.com/2.0/";
+
+    var questionsUrl = apiUrl + "questions/";
+    var answersUrl = apiUrl + "answers/";
+
+    // The different request types we are expecting
+    var expectedQuestionType = "question";
+    var expectedAnswerType = "answer";
+
+    var questionFilter = "!awRehpC1pG2C(t";
+    var answerFilter = "!6Jzry)jfEAcuO";
+
+    this.getVoteCount = function(request, renderer) {
+        request.apiUrl = getApiUrl(request);
+        request.apiFilter = getApiFilter(request);
+        queryForVoteCount(request, renderer);
+    };
+
+    function getApiUrl(request) {
+        var url = apiUrl;
+        if (request.type == expectedQuestionType) {
+            url = questionsUrl + request.id;
+        }
+        if (request.type == expectedAnswerType) {
+            url = answersUrl + request.id;
+        }
+        return url;
+    }
+
+    function getApiFilter(request) {
+        var filter = '';
+        if (request.type == expectedQuestionType) {
+            filter = questionFilter;
+        }
+        if (request.type == expectedAnswerType) {
+            filter = answerFilter;
+        }
+        return filter;
+    }
+
+    function queryForVoteCount(request, renderer) {
+        var url = request.apiUrl;
+        var apiData = {
+            site: request.host,
+            filter: request.apiFilter,
+            pagesize: 1,
+            page: 1
+        };
+        var ajaxSettings = {
+            data: apiData,
+            error: function(jqHr, status, error) {
+                console.log("Show better error message eventually");
+            },
+            success: function(data, status, jqHr) {
+                if (data.items == undefined || data.items.length == 0) {
+                    console.log("Unexpected error encountered.");
+                    return;
+                }
+                renderer(request, data);
+            },
+            url: url
+        }
+        $.ajax(ajaxSettings);
+
+    }
+}
+
+var StackApi = new StackApi();
+var SoUpDown = new SoUpDown(StackApi);
+
+SoUpDown.addExtensionClassToVoteCount();
+
+$(".so-up-and-down").on("click", function() {
+    var post = $(this).parents("table").parent("div");
+    SoUpDown.splitVoteCount(post);
 });
 
 
